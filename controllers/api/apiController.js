396938203c7c310561fb5d90c8772be98fe7b430
rpruiz33 <rpruiz33@gmail.com>
2023-02-24 19:34:52 -0300

    me parece que esta mal el formato pero termine la api usuario

--- controllers/api/apiController.js
+++ controllers/api/apiController.js
@@ -12,25 +12,34 @@ const apiController = {
         .then(usuarios => {
             let respuesta = {
                 count: usuarios.length,
-                
                 users:usuarios.map(usuario=>("id : "+usuario.usuario_id+",name : "+usuario.nombre +","+ "email : "+usuario.email+", detail: http://localhost:3030/api/users/:"+usuario.usuario_id)),
-               
             }
-                
-            
-                res.json(respuesta);
+              res.json({respuesta});
             
             })
-                
-        
-                
+                 
+        },
 
-                 
-            
-                 
+        detalle:(req, res) => {
+            db.Usuarios.findOne({where:{usuario_id:req.params.id}})
+            .then(usuario => {
+                let respuesta={
+                   user:("id : "+usuario.usuario_id+",nombre : "+usuario.nombre +","+
+                    "apellido :"+usuario.apellido+"email : "+usuario.email+"direccion : "+usuario.direccion+" localidad : "+
+                    usuario.localidad+"pais : "+usuario.pais+" edad : "+usuario.edad+" http://localhost:3030/"+usuario.imagen)
+
+                
+                }
+                res.json(respuesta);
+            })
+
+
+
+
+
         }
     }
             
     
-
+    
 module.exports = apiController;
--- routes/api/usuariosApi.js
+++ routes/api/usuariosApi.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const apiController=require('../../controllers/api/apiController')
 
 router.get('/users',apiController.total);
-
+router.get('/users/:id',apiController.detalle);
 
 
 
