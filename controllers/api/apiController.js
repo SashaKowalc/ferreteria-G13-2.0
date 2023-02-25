610d33330691e34db32604c8299f05a00c2cf447
rpruiz33 <rpruiz33@gmail.com>
2023-02-25 06:29:23 -0300

    ahora si api de usuario con su repectivo formato json

--- controllers/api/apiController.js
+++ controllers/api/apiController.js
@@ -12,7 +12,10 @@ const apiController = {
         .then(usuarios => {
             let respuesta = {
                 count: usuarios.length,
-                users:usuarios.map(usuario=>("id : "+usuario.usuario_id+",name : "+usuario.nombre +","+ "email : "+usuario.email+", detail: http://localhost:3030/api/users/:"+usuario.usuario_id)),
+                users:usuarios.map(usuario=>({"id":usuario.usuario_id,
+                "name":usuario.nombre, 
+                "email" : usuario.email, 
+                "detail": "http://localhost:3030/api/users/:"+usuario.usuario_id})),
             }
               res.json({respuesta});
             
@@ -24,10 +27,17 @@ const apiController = {
             db.Usuarios.findOne({where:{usuario_id:req.params.id}})
             .then(usuario => {
                 let respuesta={
-                   user:("id : "+usuario.usuario_id+",nombre : "+usuario.nombre +","+
-                    "apellido :"+usuario.apellido+"email : "+usuario.email+"direccion : "+usuario.direccion+" localidad : "+
-                    usuario.localidad+"pais : "+usuario.pais+" edad : "+usuario.edad+" http://localhost:3030/"+usuario.imagen)
-
+                    user:{
+                   "user id " : usuario.usuario_id,
+                   "nombre" : usuario.nombre,
+                    "apellido" :usuario.apellido,
+                    "email":usuario.email,
+                    "direccion":usuario.direccion,
+                    "localidad":usuario.localidad,
+                    "pais:":usuario.pais,
+                    "edad":usuario.edad,
+                    "imagen": "http://localhost:3030/"+usuario.imagen
+                    }
                 
                 }
                 res.json(respuesta);
